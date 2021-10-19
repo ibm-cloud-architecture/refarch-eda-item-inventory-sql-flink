5c6eacc5b178cca0f319a91a696c85524824924f
Hunter Medney <hunter_medney@us.ibm.com>
2021-10-19 00:28:20 -0400

    working sql

--- inventory.sql
+++ inventory.sql
@@ -1,17 +1,30 @@
-CREATE TABLE inventory (
-  `id` INT,
-  `price` FLOAT,
-  `quantity` INT,
-  `sku` STRING,
-  `storeName` STRING,
-  `timestamp`STRING,
-  `type` STRING,
-  `ts` TIMESTAMP(3) METADATA FROM 'timestamp'
+CREATE TABLE items (
+  id INT,
+  price FLOAT,
+  quantity INT,
+  sku STRING,
+  storeName STRING,
+  `timestamp` STRING,
+  `type` STRING
 ) WITH (
   'connector' = 'kafka',
   'topic' = 'items',
-  'properties.bootstrap.servers' = 'localhost:9092',
+  'properties.bootstrap.servers' = 'kafka:29092',
   'properties.group.id' = 'testGroup',
   'scan.startup.mode' = 'earliest-offset',
   'format' = 'json'
 );
+
+CREATE VIEW inventory AS
+SELECT
+  storeName,
+  sku,
+  COUNT(*) AS transactions,
+  AVG(CASE WHEN `type` = 'RESTOCK' THEN `price` ELSE null END) AS average_restock_price,
+  AVG(CASE WHEN `type` = 'SALE' THEN `price` ELSE null END) AS average_sale_price,
+  SUM(CASE WHEN `type` = 'SALE' THEN -`quantity` ELSE `quantity` END) AS on_hand
+FROM items
+GROUP BY storeName,sku;
+
+
+select * from inventory;
